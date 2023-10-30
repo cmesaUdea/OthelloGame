8d4ddf70e366d815d8bd543bc1600b4d3cbf80db
cmesaUdea <carlos.mesar@udea.edu.co>
2023-10-30 15:54:18 -0500

    Agregado la clase juego, para guardar partidas

--- OthelloGame/main.cpp
+++ OthelloGame/main.cpp
@@ -11,6 +11,7 @@ public:
     //Para el tamaño del tablero, el cual sera una matriz de tipo char
     static const int TAMANO = 8;
     char celdas[TAMANO][TAMANO];
+    bool movimientosPosibles[TAMANO][TAMANO];
 
     Tablero(){ //Metodo constructor de un tablero en la memoria
         for (int i = 0; i < TAMANO; i++) {
@@ -190,10 +191,112 @@ public:
 };
 
 
+class Juego {
+public:
+    Jugador jugador1, jugador2;
+    Tablero tablero;
+
+    Juego(const string& nombre1, const string& nombre2)
+        : jugador1('*'), jugador2('-'), nombreJugador1(nombre1), nombreJugador2(nombre2) {}
+
+    void jugar() {
+        int turno = 0;
+        while (true) {
+            if (turno == 0) {
+                cout << "\n" << nombreJugador1 << ", es tu turno (*)." << endl;
+            } else {
+                cout << "\n" << nombreJugador2 << ", es tu turno (-)." << endl;
+            }
+
+            tablero.marcarMovimientosPosibles((turno == 0) ? '*' : '-');
+            tablero.mostrar();
+
+            int fila, columna;
+            bool movimientoValido = false;
+            do {
+                cout << "Ingresa la fila y columna (ejemplo: 2 3): ";
+                cin >> fila;
+                if (cin.peek() == ' ' || cin.peek() == '\t') {
+                    cin.ignore();
+                    cin >> columna;
+                } else {
+                    cout << "Entrada no valida. Debes ingresar dos numeros separados por espacio. Ejemplo: (2 3)." << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    continue;
+                }
+
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Entrada no valida. Debes ingresar dos numeros separados por espacio. Intentalo de nuevo." << endl;
+                } else if (fila < 1 || fila > Tablero::TAMANO || columna < 1 || columna > Tablero::TAMANO) {
+                    cout << "Movimiento fuera de rango. Introduce numeros entre 1 y " << Tablero::TAMANO << ". Intentalo de nuevo." << endl;
+                } else if (!tablero.movimientosPosibles[fila - 1][columna - 1]) {
+                    cout << "Movimiento invalido. Intentalo de nuevo." << endl;
+                } else {
+                    movimientoValido = true;
+                }
+            } while (!movimientoValido);
+
+            tablero.realizarMovimiento(fila - 1, columna - 1, (turno == 0) ? '*' : '-');
+
+            turno = 1 - turno;
+
+            if (tablero.tableroCompleto()) {
+                break;
+            }
+        }
+
+        int contadorNegras = tablero.contarFichas('*');
+        int contadorBlancas = tablero.contarFichas('-');
+        string ganador = "Empate";
+        if (contadorNegras > contadorBlancas) {
+            ganador = nombreJugador1;
+        } else if (contadorBlancas > contadorNegras) {
+            ganador = nombreJugador2;
+        }
+
+        cout << "El juego ha terminado. Ganador: " << ganador << endl;
+        cout << "Puntuacion: " << nombreJugador1 << " (*): " << contadorNegras << ", " << nombreJugador2 << " (-): " << contadorBlancas << endl;
+
+        guardarResultado(ganador, contadorNegras, contadorBlancas);
+    }
+
+    void mostrarResultados() {
+        ifstream archivo("resultados.txt");
+        if (archivo.is_open()) {
+            string linea;
+            while (getline(archivo, linea)) {
+                cout << linea << endl;
+            }
+            archivo.close();
+        }
+    }
+
+private:
+    string nombreJugador1;
+    string nombreJugador2;
+
+    void guardarResultado(const string& ganador, int contadorNegras, int contadorBlancas) {
+        ofstream archivo("resultados.txt", ios::app);
+        if (archivo.is_open()) {
+            time_t ahora = time(0);
+            tm* infoTiempo = localtime(&ahora);
+            archivo << "Fecha: " << (infoTiempo->tm_year + 1900) << '/' << (infoTiempo->tm_mon + 1) << '/'
+                    << infoTiempo->tm_mday << " Hora: " << infoTiempo->tm_hour << ':' << infoTiempo->tm_min << endl;
+            archivo << "Jugador 1: " << nombreJugador1 << ", Jugador 2: " << nombreJugador2 << endl;
+            archivo << "Ganador: " << ganador << " (*): " << contadorNegras << ", (-): " << contadorBlancas << endl;
+            archivo.close();
+        }
+    }
+};
+
 int main() {
     //Othello game; //Metodo para llamar al juego
 
 
 
     return 0;
+
 }
